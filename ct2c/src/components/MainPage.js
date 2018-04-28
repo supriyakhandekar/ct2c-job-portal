933fc639ad06de269604aa50d09337a79a59952b
Sujay Khandekar <sskhandek@gmail.com>
2018-04-28 00:56:05 -0500

    Sujay help

--- ct2c/src/components/MainPage.js
+++ ct2c/src/components/MainPage.js
@@ -1,92 +1,100 @@
 import React, { Component } from 'react';
 import './MainPage.css';
 import './ProgressBar.css';
-import ProgressBar from'./ProgressBar.js';
+import ProgressBar from './ProgressBar.js';
 import Step1 from './step1.js';
 import Step2 from './step2.js';
 import Step3 from './step3.js';
 import Step4 from './step4.js';
 import Next_Button from './next_button.js';
-import $ from 'jquery';
+// import $ from 'jquery';
 //import ReactScrollbar from 'react-scrollbar-js'
 
-
 class MainPage extends Component {
-
-
   constructor(props) {
     super(props);
     this.state = {
-      clicks : 1,
-      bagItems :[]
+      clicks: 1,
+      bagItems: [],
     };
   }
 
-
-
-  getBagItems(list) {
+  addToCart(list) {
     //this.setState({bagItems : list});
   }
 
   incrementStep = () => {
     this.setState({ clicks: this.state.clicks + 1 });
-  }
+  };
 
   decrementStep = () => {
-
     if (this.state.clicks >= 1) {
       this.setState({ clicks: this.state.clicks - 1 });
     }
-  }
+  };
 
   //update progress bar background colors
   renderStep(step_number) {
-
     if (step_number == 5) {
-      step_number = 1
-      this.setState({ clicks: 1});
+      step_number = 1;
+      this.setState({ clicks: 1 });
     }
 
-    switch(step_number) {
+    switch (step_number) {
       case 1:
-        return <Step1 getBagItems = {this.getBagItems.bind(this)}/>
+        return (
+          <Step1
+            cart={this.state.bagItems}
+            addToCart={this.addToCart.bind(this)}
+          />
+        );
       case 2:
-        return <Step2 />
+        return <Step2 />;
       case 3:
-        return <Step3/>
+        return <Step3 />;
       case 4:
-        return <Step4/>
+        return <Step4 />;
       default:
-        return <Step1/>
-    };
+        return <Step1 />;
+    }
   }
 
-//code from https://gist.github.com/andjosh/6764939#file-animatedscrollto-js-L14
+  //code from https://gist.github.com/andjosh/6764939#file-animatedscrollto-js-L14
 
-//following smooth scroll code from
-// https://css-tricks.com/snippets/jquery/smooth-scrolling/
+  //following smooth scroll code from
+  // https://css-tricks.com/snippets/jquery/smooth-scrolling/
 
   render() {
     return (
       <div>
-      <div class='inital_page_wrapper'>
-        <div id='title'>
-          <div>C2TC</div>
-          <p class='company-motto'>Cut to the chase</p>
-          <a href='#section2' class='start-anchor'>
-            <div class='start-button'>GET STARTED</div>
-          </a>
+        <div class="inital_page_wrapper">
+          <div id="title">
+            <div>C2TC</div>
+            <p class="company-motto">Cut to the chase</p>
+            <a href="#section2" class="start-anchor">
+              <div class="start-button">GET STARTED</div>
+            </a>
+          </div>
         </div>
-      </div>
-      <div class="wrapper" id='section2'>
-        <div class='main-header'></div>
-        <ProgressBar clickCount = {this.state.clicks}/>
-        <div class='step-control-wrapper'>
-          <button class='step_button prev_button' onClick = {this.decrementStep}>PREVIOUS</button>
-          <button class='step_button next_button' onClick = {this.incrementStep}>NEXT</button>
+        <div class="wrapper" id="section2">
+          <div class="main-header" />
+          <ProgressBar clickCount={this.state.clicks} />
+          <div class="step-control-wrapper">
+            <button
+              class="step_button prev_button"
+              onClick={this.decrementStep}
+            >
+              PREVIOUS
+            </button>
+            <button
+              class="step_button next_button"
+              onClick={this.incrementStep}
+            >
+              NEXT
+            </button>
+          </div>
+          {this.renderStep(this.state.clicks)}
         </div>
-        {this.renderStep(this.state.clicks)}
-      </div>
       </div>
     );
   }
--- ct2c/src/components/step1.js
+++ ct2c/src/components/step1.js
@@ -5,83 +5,82 @@ import './step_shared.css';
 //code inspired from https://codepen.io/mtclmn/pen/QyPVJp?editors=1010
 
 class List extends Component {
-
   constructor(props) {
     super(props);
     this.clicked = this.clicked.bind(this);
   }
 
- clicked(item) {
-   this.props.addToCart(item);
- }
+  clicked(item) {
+    this.props.addToCart(item);
+  }
 
   render() {
     var clickHandler = this.clicked.bind(this);
 
     return (
-      <ul class='list-container'>
-      {
-        this.props.items.map(function(item) {
-          return <li className="list-item" data-category={item} key={item}>
-                  <div class='company-name'>{item}</div>
-                  <button class='cart-button' onClick={() => clickHandler(item)}>Add to cart</button>
-                 </li>
-        })
-       }
+      <ul class="list-container">
+        {this.props.items.map(function(item) {
+          return (
+            <li className="list-item" data-category={item} key={item}>
+              <div class="company-name">{item}</div>
+              <button class="cart-button" onClick={() => clickHandler(item)}>
+                Add to cart
+              </button>
+            </li>
+          );
+        })}
       </ul>
-    )
+    );
   }
 }
 
 class Step1 extends Component {
-
   constructor(props) {
     super(props);
-    this.state=  {
-        items : [ "Branch Metrics",
-        "Kidaptive",
-        "Farmer Analytics",
-        "Spire",
-        "Samsara",
-        "Qualtrics",
-        "Tinker",
-        "CareDash",
-        "500px",
-        "affirm",
-        "53",
-        "Glow",
-        "Groupon",
-        "Magic Leap",
-        "Rigetti",
-        "Soylent",
-        "Within",
-        "Kensho",
-        "Medium",
-        "Mesosphere"],
-        searchedItems : [],
-        selectedItems: []
-     };
-     this.addToCart = this.addToCart.bind(this);
+    this.state = {
+      items: [
+        'Branch Metrics',
+        'Kidaptive',
+        'Farmer Analytics',
+        'Spire',
+        'Samsara',
+        'Qualtrics',
+        'Tinker',
+        'CareDash',
+        '500px',
+        'affirm',
+        '53',
+        'Glow',
+        'Groupon',
+        'Magic Leap',
+        'Rigetti',
+        'Soylent',
+        'Within',
+        'Kensho',
+        'Medium',
+        'Mesosphere',
+      ],
+      searchedItems: [],
+    };
+    this.addToCart = this.addToCart.bind(this);
   }
 
   addToCart(item) {
-    var selected = this.state.selectedItems;
+    var selected = this.props.cart;
     //check if the bag already has the item
     //avoiding duplicates
     if (selected.indexOf(item) < 0) {
       selected.push(item);
     }
 
-    this.setState({selectedItems:selected});
-    var bag = this.state.selectedItems;
-    alert(`Your shopping bag has: ${bag}`)
+    this.props.addToCart(selected);
   }
 
   search(event) {
     var currentList = this.state.items,
-        newList = [],
-        searchVal = event.target.value,
-        i;
+      newList = [],
+      searchVal = event.target.value,
+      i;
 
     /*
     for (i = 0; i < currentList.length; i++) {
@@ -93,32 +92,41 @@ class Step1 extends Component {
     }
     */
 
-    currentList = currentList.filter(function(item){
-      return item.toLowerCase().search(
-        event.target.value.toLowerCase()) !== -1;
+    currentList = currentList.filter(function(item) {
+      return item.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
     });
 
-    this.setState({items: currentList});
+    this.setState({ items: currentList });
   }
 
   render() {
     return (
-      <div class='box'>
-        <div class='step-header'>
-          <div class='step_number'>1</div>
-          <div class='step_description_component'>Step 1: Find companies.</div>
+      <div class="box">
+        <div class="step-header">
+          <div class="step_number">1</div>
+          <div class="step_description_component">Step 1: Find companies.</div>
         </div>
-        <div class='step1_body'>
+        <div class="step1_body">
           <form>
-            <p class='text-style'></p>
-            <label class='text-style'>
-              <input class='search' type="text" name="name" placeholder="Magic Leap" onChange={this.search.bind(this)}/>
+            <p class="text-style" />
+            <label class="text-style">
+              <input
+                class="search"
+                type="text"
+                name="name"
+                placeholder="Magic Leap"
+                onChange={this.search.bind(this)}
+              />
             </label>
           </form>
-          <div class='search-results'>
-            <p class='text-style' id='text-style-black'>Results</p>
-            <List items={this.state.items}  addToCart={this.addToCart.bind(this)}/>
-            {this.props.getBagItems(this.state.selectedItems)}
+          <div class="search-results">
+            <p class="text-style" id="text-style-black">
+              Results
+            </p>
+            <List
+              items={this.state.items}
+              addToCart={this.addToCart.bind(this)}
+            />
           </div>
         </div>
       </div>
